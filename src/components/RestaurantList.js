fd583ce8a052d2f66be4990229e854ba0a06f8e7
filip.buchta <filip.buchta@embedit.cz>
2018-03-22 09:07:56 +0100

    now

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -7,16 +7,14 @@ import {visit} from "./../actions"
 class Food extends Component {
     render() {
         return (
-            <ul>
-                <li>
+            <span>
                     {this.props.food.isSoup ?
                         <span><span className="badge badge-pill badge-default">Polévka</span>&nbsp;</span> : ""}
-                    {this.props.food.name}
-                    &nbsp;
-                    {this.props.likedBy.map((value, index) => <span key={index}><span
-                        className={"badge badge-pill " + ((this.props.user.id == value.id) ? "badge-warning" : "badge-success")}>{value.nickname}</span>&nbsp;</span>)}
-                </li>
-            </ul>
+                <span dangerouslySetInnerHTML={{__html: this.props.food.name}}/>
+                &nbsp;
+                {this.props.likedBy.map((value, index) => <span key={index}><span
+                    className={"badge badge-pill " + ((this.props.user.id == value.id) ? "badge-warning" : "badge-success")}>{value.nickname}</span>&nbsp;</span>)}
+             </span>
         );
     }
 }
@@ -29,17 +27,17 @@ class Restaurant extends Component {
         let today = new Date().toISOString().split("T")[0];
 
         let visitedToday = this.props.visits.some(v => v.id === today && v.restaurantId === this.props.restaurant.id);
-        let lastVisit = this.props.visits.filter( v => v.restaurantId === this.props.restaurant.id ).sort( (a, b) => Date.parse(b.id) - Date.parse(a.id) )[0];
+        let lastVisit = this.props.visits.filter(v => v.restaurantId === this.props.restaurant.id).sort((a, b) => Date.parse(b.id) - Date.parse(a.id))[0];
         let lastVisitDays = lastVisit == null ? -1 : Math.floor(Math.abs(Date.now() - Date.parse(lastVisit.id)) / (1000 * 3600 * 24));
         return (
             <div className="row">
                 <div className="col-lg-12">
                     <div className="pull-right">
-                        { lastVisitDays > 0 ? <small className="text-muted">{lastVisitDays + " days ago"}</small>  : "" }
-                            &nbsp;
-                        { canVisit ?
+                        {lastVisitDays > 0 ? <small className="text-muted">{lastVisitDays + " days ago"}</small> : ""}
+                        &nbsp;
+                        {canVisit ?
                             <button className={"btn btn-outline-primary"} onClick={this.props.onVisit}>
-                                <i className={"fa " + (visitedToday ? "fa-check-square-o" : "fa-square-o") }/>
+                                <i className={"fa " + (visitedToday ? "fa-check-square-o" : "fa-square-o")}/>
                             </button> : ""
                         }
                     </div>
@@ -47,13 +45,21 @@ class Restaurant extends Component {
                         <a href={this.props.restaurant.url}>{this.props.restaurant.name}</a>
                     </h3>
                     <div>
-                        {this.props.restaurant.foods
-                            .map((value, index) => {
+                        <ul>
+                            {this.props.restaurant.foods
+                                .map((value, index) => {
                                     let likedBy = this.props.users.filter(u => u.preferences.split('\n').some(p => value.name.match(new RegExp(p, "i"))));
 
-                                    return <Food key={index} food={value} user={this.props.user} likedBy={likedBy}/>
-                                }
-                            )}
+
+                                    let food = <Food key={index} food={value} user={this.props.user} likedBy={likedBy}/>;
+
+                                    if (this.props.restaurant.foods.length > 1)
+                                        return <li>{food}</li>;
+                                    else
+                                        return food;
+
+                                })}
+                        </ul>
                     </div>
                     <br/>
                 </div>
@@ -67,19 +73,19 @@ class RestaurantList extends Component {
 
         let visits = this.props.group == null ? [] : this.props.group.visits;
 
-        let users = this.props.group == null ? [] : this.props.users.filter( (user) => user.groupId == this.props.user.groupId );
+        let users = this.props.group == null ? [] : this.props.users.filter((user) => user.groupId == this.props.user.groupId);
 
 
         return (<div className="row">
             <div className="col-lg-12">
                 {this.props.restaurants
-                    //.sort((a, b) => visits.filter( v => v.restaurantId == b.id).length - visits.filter( v => v.restaurantId == a.id).length)
-                    .filter( restaurant => this.props.group == null || this.props.group.restaurants.some( r => r.id == restaurant.id ))
+                //.sort((a, b) => visits.filter( v => v.restaurantId == b.id).length - visits.filter( v => v.restaurantId == a.id).length)
+                    .filter(restaurant => this.props.group == null || this.props.group.restaurants.some(r => r.id == restaurant.id))
                     .map((restaurant, index) => <Restaurant key={index} user={this.props.user} restaurant={restaurant}
-                                                       users={users} visits={visits}
-                                                       onVisit={() => {
-                                                           this.props.onVisit(restaurant);
-                                                       }}/>)}
+                                                            users={users} visits={visits}
+                                                            onVisit={() => {
+                                                                this.props.onVisit(restaurant);
+                                                            }}/>)}
             </div>
         </div>)
     }
@@ -91,7 +97,7 @@ export default connect(
             user: state.authentication.user,
             restaurants: state.restaurants.list,
             users: state.users.list,
-            group: state.authentication.user == null ? null : state.groups.list.find( g => g.id === state.authentication.user.groupId)
+            group: state.authentication.user == null ? null : state.groups.list.find(g => g.id === state.authentication.user.groupId)
         }
     },
     (dispatch, ownProps) => {
--- src/parsers/eva.js
+++ src/parsers/eva.js
@@ -1,6 +1,5 @@
 export default (restaurant) => {
     let menu = [];
-    menu.push({name: "Velmi červené rizoto s ajdamem", period: "permament"});
-    menu.push({name: "Krém Bryléé bez bílků", period: "permament"});
+    menu.push({name: "<img src='/smazak.png' />", period: "permament"});
     return Promise.resolve(menu);
 }
